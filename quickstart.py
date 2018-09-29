d203ef767b36e48c870db348b07137536fe85439
Henry Qin <root@hq6.me>
2018-09-29 00:36:37 -0700

    Success at updating slide numbers across the figures

--- quickstart.py
+++ quickstart.py
@@ -6,15 +6,19 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = 'https://www.googleapis.com/auth/presentations.readonly'
+# SCOPES = 'https://www.googleapis.com/auth/presentations.readonly'
+SCOPES = 'https://www.googleapis.com/auth/presentations'
 
 # The ID of a sample presentation.
 PRESENTATION_ID = '1CItHgsDlA1Sbz_g4zXh678C5_qCiAtOf7mKVQH9rYiI'
 
 def extractIdOfSlideNum(pageElements):
     for element in pageElements:
+      try:
 	if element['shape']['text']['textElements'][1]['autoText']['type'] == u'SLIDE_NUMBER':
 	    return element['objectId']
+      except:
+        pass
 
 
 def main():
@@ -32,18 +36,32 @@ def main():
     presentation = service.presentations().get(
         presentationId=PRESENTATION_ID).execute()
     slides = presentation.get('slides')
+    # return slides
 
+    requests = []
     print('The presentation contains {} slides:'.format(len(slides)))
     for i, slide in enumerate(slides):
         print('- Slide #{} contains {} elements.'.format(
             i + 1, len(slide.get('pageElements'))))
 	# See if we can find the element that includes the page number
-	pageNumId = None
-	try:
-	    pageNumId = extractIdOfSlideNum(slide.get('pageElements'))
-	except:
-	    pass
-	print(pageNumId)
+	pageNumId = extractIdOfSlideNum(slide.get('pageElements'))
+	# print(pageNumId)
+
+        if pageNumId:
+            request = {}
+            request['updateTextStyle'] = {}
+            request['updateTextStyle']['objectId'] = pageNumId
+            request['updateTextStyle']['textRange'] = { 'type': 'ALL' }
+            request['updateTextStyle']['style'] = { 'fontSize': {'magnitude' : 14, 'unit' : 'PT'}}
+            request['updateTextStyle']['fields'] = 'fontSize'
+            requests.append(request) 
+
+    # Send request
+    body = { 'requests': requests }
+    response = service.presentations().batchUpdate(
+                presentationId=PRESENTATION_ID, body=body).execute()
+
+
 
 if __name__ == '__main__':
     main()
